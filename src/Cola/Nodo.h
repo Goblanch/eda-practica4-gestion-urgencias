ebddca8658440f821b9c85626e10a7e95a57e0f1
Gonzalo Blanch Dominguez <gonzaloblanchdev@gmail.com>
2026-03-24 11:34:03 +0100

    Implementar Nodo.h

--- src/Cola/Nodo.h
+++ src/Cola/Nodo.h
@@ -1,2 +1,14 @@
 #pragma once
+#include <memory>
 
+template <typename T>
+class Nodo {
+private:
+    T dato;
+    std::shared_ptr<Nodo<T>> sig;
+
+public:
+    const T& getDato() const {
+        return dato;
+    }
+};
