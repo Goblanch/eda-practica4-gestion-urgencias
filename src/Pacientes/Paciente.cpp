b12842ca5fa04f9c88593d3ab3f70b7f94a6792e
ddaniwey <ddaniwey@gmail.com>
2026-03-24 11:48:26 +0100

    Paciente.h y .cpp acabados

--- src/Pacientes/Paciente.cpp
+++ src/Pacientes/Paciente.cpp
@@ -1,1 +1,45 @@
 #include "Paciente.h"
+
+void Paciente::setNumeroHistorial(int num)
+{
+    num_historial = num;
+    actualizarCodigo();
+}
+void Paciente::setHoraLlegada(int hora)
+{
+    hora_llegada = hora;
+    actualizarCodigo();
+}
+void Paciente::setTriaje(char tri)
+{
+    triaje = tri;
+}
+void Paciente::setTemperatura(double temp)
+{
+    temperatura = temp;
+}
+void Paciente::actualizarCodigo()
+{
+    codigo = std::to_string(num_historial) +   " - " + std::to_string(hora_llegada);
+}
+int Paciente::getNumeroHistorial()
+{
+    return num_historial;
+}
+int Paciente::getHoraLlegada()
+{
+    return hora_llegada;
+}
+std::string Paciente::getCodigo()
+{
+    return codigo;
+}
+char Paciente::getTriaje()
+{
+    return triaje;
+}
+double Paciente::getTemperatura()
+{
+    return temperatura;
+}
+
--- src/Pacientes/Paciente.h
+++ src/Pacientes/Paciente.h
@@ -1,4 +1,31 @@
 #pragma once
+#include <string>
 
 class Paciente {
+private:
+    std::string codigo; // No se valida
+    int num_historial;
+    int hora_llegada; //formato hhmm
+    char triaje; // v, a, r No hay que validarlo
+    double temperatura;
+public:
+    Paciente(int num, int hora, char tri, double temp)
+    {
+        num_historial = num;
+        hora_llegada = hora;
+        triaje = tri;
+        temperatura = temp;
+        actualizarCodigo();
+    }
+    void setNumeroHistorial(int num);
+    void setHoraLlegada(int hora);
+    void setTriaje(char tri);
+    void setTemperatura(double temp);
+    void actualizarCodigo();
+    int getNumeroHistorial();
+    int getHoraLlegada();
+    std::string getCodigo();
+    char getTriaje();
+    double getTemperatura();
+
 };
