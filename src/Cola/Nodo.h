e3bd0120feecce5746e6e74ab7f585868bea66ed
Gonzalo Blanch Dominguez <gonzaloblanchdev@gmail.com>
2026-03-24 11:53:40 +0100

    Añadir funcionalidad al Nodo.

--- src/Cola/Nodo.h
+++ src/Cola/Nodo.h
@@ -8,7 +8,21 @@ private:
     std::shared_ptr<Nodo<T>> sig;
 
 public:
+    Nodo() : dato(), sig(nullptr) {}
+
+    Nodo(const T& dato) : dato(dato), sig(nullptr) {}
+
+    Nodo(const T& dato, std::shared_ptr<Nodo<T>> sig) : dato(dato), sig(sig) {}
+
     const T& getDato() const {
         return dato;
+    }
+
+    std::shared_ptr<Nodo<T>> getSig() const {
+        return sig;
+    }
+
+    void setSig(std::shared_ptr<Nodo<T>> siguiente) {
+        sig = siguiente;
     }
 };
